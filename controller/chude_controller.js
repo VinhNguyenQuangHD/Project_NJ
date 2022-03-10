396641c3076e352729510503987313c13110141f
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-03-10 20:13:17 +0700

    them va sua lai mot so trang nhat dinh

---
+++ controller/chude_controller.js
@@ -0,0 +1,62 @@
+var chude_db = require('../models/Chu_de');
+
+exports.topic_create = (req,res) =>{
+    if(!req.body){
+        res.status(400).send({message: "Form khong duoc de trong"}); 
+        return;
+    }else{
+        const new_topic = new chude_db({
+            id_chude: req.body.idchude,
+            Chu_de: req.body.tenchude,
+        });
+
+        new_topic.save(new_topic).then(data =>{
+            res.redirect('/adminpage/topics');
+        }).catch(err =>{
+            res.status(500).send({
+                message: err.message || "Cannot create new topic"
+            });
+            
+        });
+    }
+}
+
+exports.topic_read = (req,res) =>{
+    chude_db.find().then(topic =>{
+        res.send(topic)
+    }).catch(err => {
+        res.status(500).send({message: err.message || "Khong tim thay bat cu chu de nao"})
+    })
+}
+
+exports.topic_update = (req,res) =>{
+    if(!req.body){
+        res.status(400).send({message: "Form khong duoc de trong"}); 
+        return;
+    }else{
+        const id = req.param.id;
+        chude_db.findByIdAndUpdate(id, req.body, {useFindAndModify: false}).then(data =>{
+            if(!data){
+                res.status(404).send({message: `Khong the cap nhat chu de cua id ${id}`});
+            }else{
+                res.send(data);
+                res.redirect('/adminpage/topics');
+            }
+        }).catch(err =>{
+            res.status(500).send({message : err.message || "Co loi xay ra"});
+        });
+    }
+}
+
+exports.topic_delete = (req,res) =>{
+        const id = req.param.id;
+        chude_db.findByIdAndDelete(id).then(data =>{
+            if(!data){
+                res.status(404).send({message: `Khong the xoa chu de cua id ${id}`});
+            }else{
+                res.send({message: "Chu de da bi xoa"});
+            }
+        }).catch(err =>{
+            res.status(500).send({message : err.message || "Co loi xay ra"});
+        });
+}
---
+++ controller/loaisp_controller.js
@@ -0,0 +1,60 @@
+var loai_sp_db = require('../models/Loai_sp');
+
+exports.create_new_produce_type = (req,res) =>{
+    if(!req.body){
+        res.status(404).send({message : "Form khong duoc de trong"});
+    }else{
+        const new_type_product = new loai_sp_db({
+            Ma_Loai: req.body.maloai,
+            Ten_loai: req.body.tenloai,
+        });
+
+        new_type_product.save(new_type_product).then(data =>{
+            res.redirect('/adminpage/producttype');
+        }).catch(err =>{
+            res.status(500).send({message: err.message || "Khong the them moi laoi san pham"});
+        });
+    }
+}
+
+exports.read_new_produce_type = (req,res) => {
+    loai_sp_db.find().then(protype =>{
+        res.send(protype);
+    }).catch(err => {
+        res.status(500).send({message: err.message || "Khong tim thay du lieu de xem"});
+    });
+}
+
+exports.update_new_produce_type = (req,res) =>{
+    if(!req.body){
+        res.status(404).send({
+            message: "Khong the de form trong"
+        });
+    }else{
+        const id = req.params.id;
+        loai_sp_db.findByIdAndUpdate(id,req.body, {useFindAndModify: false}).then(data =>{
+            if(!data){
+                res.status(404).send({message: "Form phai duoc lap day"});
+            }else{
+                res.send(data);
+                res.redirect('/adminpage/producttype');
+            }
+        }).catch(err =>{
+            res.status(500).send({message: err.message || "Da co loi xay ra, khong the cap nhat"});
+        });
+    }
+}
+
+exports.delete_new_produce_type = (req,res) =>{
+        const id = req.params.id;
+        loai_sp_db.findByIdAndDelete(id).then(data =>{
+            if(!data){
+                res.status(404).send({message: "Form phai duoc lap day"});
+            }else{
+                res.send(data);
+                res.redirect('/adminpage/producttype');
+            }
+        }).catch(err =>{
+            res.status(500).send({message: err.message || "Da co loi xay ra, khong the cap nhat"});
+        });
+}
